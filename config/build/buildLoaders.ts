6019002a6b916c5d60851b179fd18ca2fba2340d
Sergei Solodzhuk <solodzh@gmail.com>
2023-02-18 22:51:59 +0200

    11

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,3 +1,4 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import { buildCssLoader } from './loaders/buildCssLoader';
 import { BuildOptions } from './types/config';
@@ -28,7 +29,24 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         },
     };
 
-    const cssLoader = buildCssLoader(isDev);
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]',
+                    },
+                },
+            },
+            'sass-loader',
+        ],
+    };
 
     // Если не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
