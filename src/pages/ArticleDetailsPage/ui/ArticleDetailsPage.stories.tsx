e4775320245a164e999f141cf16cc75ee32328a4
Sergei Solodzhuk <solodzh@gmail.com>
2023-04-18 02:34:22 +0200

    add parameters to RouterDecorator

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage.stories.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage.stories.tsx
@@ -77,6 +77,12 @@ const articleData: Article = {
 export default {
   title: 'pages/ArticleDetailsPage',
   component: ArticleDetailsPage,
+  parameters: {
+    router: {
+      path: '/articles/:id',
+      route: '/articles/1',
+    },
+  },
   decorators: [StoreDecorator({
     article: {
       articleData,
--- src/shared/config/storyBook/RouterDecorator/RouterDecorator.tsx
+++ src/shared/config/storyBook/RouterDecorator/RouterDecorator.tsx
@@ -1,9 +1,23 @@
-import { Story } from '@storybook/react';
+import { Story, StoryContext } from '@storybook/react';
 import 'app/styles/index.scss';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  BrowserRouter, MemoryRouter, Route, Routes,
+} from 'react-router-dom';
 
-export const RouterDecorator = (StoryComponent: Story) => (
-  <BrowserRouter>
-    <StoryComponent />
-  </BrowserRouter>
-);
+export const RouterDecorator = (
+  StoryComponent: Story,
+  { parameters: { router } }: StoryContext,
+) => (
+  router
+    ? (
+      <MemoryRouter initialEntries={[encodeURI(router.route)]}>
+        <Routes>
+          <Route path={router.path} element={<StoryComponent />} />
+        </Routes>
+      </MemoryRouter>
+    )
+    : (
+      <BrowserRouter>
+        <StoryComponent />
+      </BrowserRouter>
+    ));
